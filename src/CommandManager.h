7cfb035e41f7b15166f7fe474a16cd5db6a3238c
laraibfatimamughal07 <laraibfatimamughal07@gmail.com>
2026-05-08 13:34:40 +0500

    corrected CommandManager.h (added all remaining commands)

--- src/CommandManager.h
+++ src/CommandManager.h
@@ -2,9 +2,17 @@
 #include "Folder.h"
 class commandManager {
 	Folder* current;
+	Folder* root;
 public:
 	commandManager(Folder* root);
+	void Execute(string command);
 
-	void run();
-	void Execute(string i);
+	void ls();
+	void mkdir(string name);
+	void touch(string type, string name);
+	void cd(string name);
+	void search(string name);
+	void rm(string name);
+	void renameNode(string newName);
+
 };
